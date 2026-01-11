b8d904373af13d5d1b8fda5dffc2ac63b5b732f2
Alexsandro <alexsandrocosta855@gmail.com>
2026-01-11 15:12:11 -0300

    Resolution for problem 27
    Use Math.ceil as this is not cljs code

--- src/core.clj
+++ src/core.clj
@@ -26,3 +26,21 @@
   (take n (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))))
 
 (fibonacci 3)
+
+
+;; Problem 27, Palindrome Detector
+
+(defn palindrome-detector
+  [s]
+  (let [mid-point (Math/ceil (/ (count s) 2))
+        ; Need to use js/Math.ceil above when sending this function as the result, 
+        ; 4clojure doesn't recognize Math/ceil.
+        splitted-sequence (split-at mid-point s)
+        last-part (last splitted-sequence)
+        first-part (first splitted-sequence)
+        first-part (if (not (= (count last-part) (count first-part)))
+                     (butlast first-part)
+                     first-part)]
+    (= first-part (reverse last-part))))
+
+(palindrome-detector '(1 2 37 6 3 2 1))
